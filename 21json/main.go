ac881c27ab4a7d9e8b0f3fd9b1875a3e1aaf52a7
Prasoon Soni <prasoonsoni03@gmail.com>
2023-05-24 17:21:33 +0530

    Added 21json - Decode

--- 21json/main.go
+++ 21json/main.go
@@ -8,13 +8,14 @@ import (
 type user struct {
 	Name     string   `json:"username"`
 	Email    string   `json:"useremail"`
-	Password string   `json:"-"` // here - is given so that nobody is able to consume this field
+	Password string   `json:"-"`                   // here - is given so that nobody is able to consume this field
 	Posts    []string `json:"userposts,omitempty"` // omitempty ignores empty fields
 }
 
 func main() {
 	fmt.Println("Welcome to JSON")
-	EncodeJson()
+	// EncodeJson()
+	DecodeJson()
 }
 
 func EncodeJson() {
@@ -30,4 +31,35 @@ func EncodeJson() {
 		panic(err)
 	}
 	fmt.Println(string(finalJson))
+}
+
+func DecodeJson() {
+	jsonData := []byte(`
+	{
+                "username": "Prasoon",
+                "useremail": "prasoon@gmail.com",
+                "userposts": ["1", "2", "3"]
+    }
+	`)
+
+	var users user
+	checkValid := json.Valid(jsonData)
+
+	if checkValid {
+		fmt.Println("JSON Valid")
+		json.Unmarshal(jsonData, &users)
+		fmt.Printf("%#v\n", users)
+	} else {
+		fmt.Println("JSON WAS NOT VALID")
+	}
+
+	// some cases where you just want to add data to key value pair
+
+	var myUserData map[string]interface{}
+	json.Unmarshal(jsonData, &myUserData)
+	fmt.Println(myUserData)
+
+	for k, v := range myUserData {
+		fmt.Printf("Key is %v and Value is %v\n", k, v)
+	}
 }
